ca8f5a7671716fde8db92091b90a382050eda2f8
Maksym Kaban <maksym.kaban@gmail.com>
2022-01-21 22:53:45 +0200

    exec pathes modifications

--- .emacs.d/rc/emacs-rc-golang.el
+++ .emacs.d/rc/emacs-rc-golang.el
@@ -6,13 +6,13 @@
 
 ;;;; Code:
 (setq exec-path (append '("/usr/lib/go/bin") exec-path))
-(setenv "PATH" (concat "/usr/lib/go/bin:" (getenv "PATH")))
+
+;;(setenv "PATH" (concat "/usr/lib/go/bin:" (getenv "PATH")))
 
 (use-package go-mode
-  :load-path "/home/mkaban/playground/go/src/golang.org/x/lint/misc/emacs"
+  :load-path  "/home/mkaban/playground/go/src/golang.org/x/lint/misc/emacs"
   
   :config
-  (setenv "GOPATH" "/home/maksym_kaban/go")
   (use-package golint)
   (projectile-global-mode 1)
   (projectile-mode +1)
@@ -30,6 +30,10 @@
   (use-package lsp-mode
     :commands (lsp lsp-deferred)
     :hook (go-mode . lsp-deferred))
+  ;; Optional - provides fancier overlays.
+  (use-package lsp-ui
+    :ensure t
+    :commands lsp-ui-mode)
   (use-package yasnippet
     :ensure t
     :commands yas-minor-mode
@@ -89,10 +93,10 @@
 ;;                           (company-mode)
 ;;                           (set (make-local-variable 'company-backends) '(company-go))))
 
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands (lsp lsp-deferred)
+;;   :hook (go-mode . lsp-deferred))
 
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
@@ -101,10 +105,6 @@
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
-;; Optional - provides fancier overlays.
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode)
 
 ;; Company mode is a standard completion package that works well with lsp-mode.
 ;; (use-package company
